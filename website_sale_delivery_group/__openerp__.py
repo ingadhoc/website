e8131c4d9567d3544030985e51d467e7680e410e
Juan Jose Scarafia <jjs@adhoc.com.ar>
2016-03-01 13:37:01 -0300

    ADD description

--- website_sale_delivery_group/__openerp__.py
+++ website_sale_delivery_group/__openerp__.py
@@ -31,6 +31,8 @@
         'website_sale_delivery'
     ],
     'installable': True,
+    'description': 'Allow to set groups on delivery methods acquires, '
+    'only users of that groups will be able to use them',
     'name': 'e-Commerce Delivery Groups',
     'test': [],
     'version': '8.0.0.0.0',
