15635c85140f9b9ebf63be1a4ab9c9da3a13ed28
Juan Jose Scarafia <jjs@adhoc.com.ar>
2016-03-11 09:39:27 -0300

    IMP custom search

--- website_sale_custom_search/controllers/main.py
+++ website_sale_custom_search/controllers/main.py
@@ -5,6 +5,9 @@
 ##############################################################################
 from openerp.addons.website_sale.controllers.main import website_sale
 from openerp.http import request
+from openerp import http
+from openerp.addons.website_sale.controllers import main as main_file
+import werkzeug
 
 
 class website_sale(website_sale):
@@ -18,8 +21,45 @@ class website_sale(website_sale):
             for srch in search.split(" "):
                 public_categ_ids = pool['product.public.category'].search(
                     cr, uid, [('name', 'ilike', srch)], context=context)
-                domain = ['|'] + domain + [
-                    '|', ('attribute_line_ids.value_ids.name', 'ilike', srch),
-                    ('public_categ_ids', 'child_of', public_categ_ids)]
+                domain = [
+                    '|', ('sale_ok', '=', True), '|',
+                    ('attribute_line_ids.value_ids.name', 'ilike', srch),
+                    ('public_categ_ids', 'child_of', public_categ_ids)
+                    ] + domain
         return domain
-# vim:expandtab:tabstop=4:softtabstop=4:shiftwidth=4:
+
+    @http.route()
+    def shop(self, page=0, category=None, search='', **post):
+        """
+        If we have a search and category, clean category
+        """
+        if category and search:
+            category = None
+        return super(website_sale, self).shop(page, category, search, **post)
+
+
+class QueryURL(object):
+    def __init__(self, path='', **args):
+        self.path = path
+        self.args = args
+
+    def __call__(self, path=None, **kw):
+        if not path:
+            path = self.path
+        is_category = path.startswith('/shop/category/')
+        for k, v in self.args.items():
+            if is_category and k == 'search':
+                continue
+            kw.setdefault(k, v)
+        l = []
+        for k, v in kw.items():
+            if v:
+                if isinstance(v, list) or isinstance(v, set):
+                    l.append(werkzeug.url_encode([(k, i) for i in v]))
+                else:
+                    l.append(werkzeug.url_encode([(k, v)]))
+        if l:
+            path += '?' + '&'.join(l)
+        return path
+
+main_file.QueryURL = QueryURL
