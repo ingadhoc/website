f123c2bb9ea16bd787b7eef943edf0b99ff2b433
Juan Jose Scarafia <jjs@adhoc.com.ar>
2016-05-18 20:39:54 -0300

    ADD todopago

---
+++ payment_todopago/README.rst
@@ -0,0 +1,47 @@
+payment_todopago
+================
+TodoPago payment module for Odoo 8.0 Ecommerce
+
+Configuration
+-------------
+1. Install this module
+2. Go on to config / payment / payment methods / todpago
+3. If you want to use it on sales order you should set "validation" to "automatic" (or you can create to payment adquires). The issue is that with "automatic", on payment return with status "pending", no message is displayed to the user regarding pending payment. (still not working)
+
+Notes:
+* "Mensaje de agradecimiento" on payment adquires is only displayed if payment is pending and adquires is of validation type "manual".
+
+Known issues / Roadmap
+----------------------
+1. improove return url, we should send it to mercadopago and have return data. perhups we can use additional_info and read it from mercadopago on response. Check mercadopago_back_no_return on 'main'
+2. If we choose to use validation = automatic, perhups we can post a message if validation is on pending (we should overwrite "payment_get_status")
+3. Implement ipn from https://www.mercadopago.com.ar/developers/es/api-docs/basic-checkout/ipn/
+4. When paying from portal, not transaction is create do we get an error  with _mercadopago_form_get_tx_from_data method, we should check how other payment methods works
+
+TEST MODE Y DESARROLLo
+=========
+Crear cuentas en:
+https://todopago.com.ar/
+Entrar a herramietnas / comercios: integración y generar credeciales ya sea para ambiente de producción o ambiente de pruebas
+
+Mas info en https://developers.todopago.com.ar/site/
+
+Información de SANDBOX:
+    'Authorization' = 'PRISMA A793D307441615AF6AAAD7497A75DE59'
+    'SECURITY' = 'A793D307441615AF6AAAD7497A75DE59'
+    'CURRENCYCODE' = 032
+    'MERCHANT' = 2159
+    'ENCODINGMETHOD' = 'XML'
+
+Y
+    'END_POINT' = "https://developers.todopago.com.ar/services/t/1.1/"
+    'Authorize' = "https://developers.todopago.com.ar/services/t/1.1/Authorize?wsdl"
+    'PaymentMethods' = "https://developers.todopago.com.ar/services/t/1.1/PaymentMethods?wsdl"
+    'Operations' = "https://developers.todopago.com.ar/services/t/1.1/Operations?wsdl"
+
+Datos adicionales para contrl de fraude:
+https://github.com/TodoPago/SDK-Python#datos-adicionales-para-control-de-fraude
+
+Formas de pagar:
+----------------
+Datos de prueba desde https://developers.todopago.com.ar/site/datos-de-prueba
---
+++ payment_todopago/controllers/main.py
@@ -0,0 +1,110 @@
+# -*- coding: utf-8 -*-
+##############################################################################
+# For copyright and license notices, see __openerp__.py file in module root
+# directory
+##############################################################################
+
+import logging
+import pprint
+import werkzeug
+from openerp import http, SUPERUSER_ID
+from openerp.http import request
+from ast import literal_eval
+_logger = logging.getLogger(__name__)
+
+
+class TodoPagoController(http.Controller):
+    _success_url = '/payment/todopago/success'
+    _success_no_return_url = '/payment/todopago/success_no_return'
+    _failure_no_return_url = '/payment/todopago/failure_no_return'
+    _failure_url = '/payment/todopago/failure'
+    _create_preference_url = '/payment/todopago/create_preference'
+
+    @http.route([
+        '/payment/todopago/create_preference',
+    ],
+        type='http', auth="none")
+    def todopago_create_preference(self, **post):
+        _logger.info(
+            'todopago: create preference with post data %s',
+            pprint.pformat(post))
+        # TODO podriamos pasar cada elemento por separado para no necesitar
+        # el literal eval
+        todopago_data = literal_eval(post.get('todopago_data', {}))
+        if not todopago_data:
+            return werkzeug.utils.redirect("/")
+        acquirer_id = todopago_data.get('acquirer_id')
+        cr, uid, context = request.cr, SUPERUSER_ID, request.context
+        request_url = request.registry[
+            'payment.acquirer']._todopago_create_transaction(
+            cr, uid, acquirer_id, post, context)
+        return werkzeug.utils.redirect(request_url)
+
+    def todopago_validate(self, **post):
+        "Validate mercado pago payment from a return URL or IPN"
+        _logger.info(
+            'Validating todopago payment with post data %s',
+            pprint.pformat(post))
+        cr, uid, context = request.cr, SUPERUSER_ID, request.context
+        request.registry['payment.transaction'].form_feedback(
+            cr, uid, post, 'todopago', context)
+        return False
+
+    @http.route([
+        '/payment/todopago/success_no_return',
+        '/payment/todopago/failure_no_return',
+    ],
+        type='http', auth="none")
+    def todopago_back_no_return(self, **post):
+        """
+        Odoo, si usas el boton de pago desde una sale order o email, no manda
+        una return url, desde website si y la almacenan en un valor que vuelve
+        desde el agente de pago. Como no podemos mandar esta "return_url" para
+        que vuelva, directamente usamos dos distintas y vovemos con una u otra
+        """
+        # TODO nos falta implementar esto, en realidad deberia volver a la
+        # orden de venta o a donde?
+        _logger.info(
+            'todopago: entering mecadopago_back with post data %s',
+            pprint.pformat(post))
+        self.todopago_validate(**post)
+        return werkzeug.utils.redirect('/')
+
+    @http.route([
+        '/payment/todopago/success',
+        '/payment/todopago/failure',
+    ],
+        type='http', auth="none")
+    def todopago_back(self, **post):
+        _logger.info(
+            'todopago: entering mecadopago_back with post data %s',
+            pprint.pformat(post))
+        self.todopago_validate(**post)
+        return werkzeug.utils.redirect('/shop/payment/validate')
+
+    # @http.route([
+    #     '/payment/todopago/failure'
+    # ],
+    #     type='http', auth="none")
+    # def todopago_back_failure(self, **post):
+    #     """
+    #     If failure is return is because user has cancelled the payment
+    #     if a collection_status has return, then we call todopago_validate
+    #     to post payment failure reason.
+    #     If no collection_status user has return before triying to pay
+    #     """
+    #     _logger.info(
+    #         'todopago: entering todopago_back_failure with post data %s',
+    #         pprint.pformat(post))
+    #     # por ahora, para simplificar, siempre que se cancele devolvemos
+    #     # a la pestana de pagos, ver observaciones de mas abajo
+    #     return werkzeug.utils.redirect('/shop/payment')
+        # # TODO para seguir con la logica de otros modulos deberiamos
+        # # redirigir a la redirect url y que ahí decida que hacer
+        # # el problema es que si gregresamos a esa url se nos esta limpiando
+        # # el pedido
+        # # return werkzeug.utils.redirect('/shop/payment/validate')
+        # if post.get('collection_status') == 'null':
+        #     return werkzeug.utils.redirect('/shop/payment')
+        # self.todopago_validate(**post)
+        # return werkzeug.utils.redirect('/shop/payment/validate')
