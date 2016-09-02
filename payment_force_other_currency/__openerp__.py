a3e848e8212f7da8743eb472406aebddbc0a588c
susport <richard.pompa84@gmail.com>
2016-09-02 00:25:01 -0300

    9.0-mig-payment_force_other_currency
    Payment Force Other Currency

--- payment_force_other_currency/__openerp__.py
+++ payment_force_other_currency/__openerp__.py
@@ -23,10 +23,6 @@
     'category': 'Website',
     'summary': 'Website Sale',
     'version': '9.0.1.0.0',
-    'description': '''
-Payment Force Other Currency
-============================
-        ''',
     'author': 'ADHOC SA',
     'website': 'www.adhoc.com.ar',
     'license': 'AGPL-3',
@@ -38,6 +34,6 @@ Payment Force Other Currency
     ],
     'demo': [
     ],
-    'installable': False,
+    'installable': True,
     'application': False,
 }
