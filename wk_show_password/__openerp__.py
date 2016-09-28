db1ed4b459d77ca6442b036b890d2e40a34af734
Juan Jose Scarafia <jjs@adhoc.com.ar>
2016-09-28 16:40:43 -0300

    REF

--- wk_show_password/__openerp__.py
+++ wk_show_password/__openerp__.py
@@ -1,9 +1,10 @@
 # -*- coding: utf-8 -*-
-#################################################################################
+##########################################################################
 #
-#   Copyright (c) 2015-Present Webkul Software Pvt. Ltd. (<https://webkul.com/>)
-#   See LICENSE file for full copyright and licensing details.
-#################################################################################
+#  Copyright (c) 2015-Present Webkul Software Pvt. Ltd. (<https://webkul.com/>)
+#  See LICENSE file for full copyright and licensing details.
+#
+##########################################################################
 {
     "name": "Website Show Password",
     "category": 'Website',
@@ -17,8 +18,6 @@
 .. |icon_features| image:: wk_show_password/static/src/img/icon-features.png
 .. |icon_support| image:: wk_show_password/static/src/img/icon-support.png
 .. |icon_help| image:: wk_show_password/static/src/img/icon-help.png
-
-|icon_help| `Help <https://webkul.com/ticket/open.php>`_ |icon_support| `Support <https://webkul.com/ticket/open.php>`_ |icon_features| `Request new Feature(s) <https://webkul.com/ticket/open.php>`_
     """,
     "author": "Webkul Software Pvt. Ltd.",
     "website": "http://www.webkul.com",
@@ -30,5 +29,5 @@
     "installable": True,
     "application": True,
     "auto_install": False,
-    "images":['static/description/Banner.png']
+    "images": ['static/description/Banner.png']
 }
