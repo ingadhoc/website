ae4bd37639efe96ea37dfa90b27c8dedb7f608a0
Juan Jose Scarafia <jjs@adhoc.com.ar>
2017-01-18 10:22:32 -0300

    ADD mig scripts

--- payment_mercadopago/__openerp__.py
+++ payment_mercadopago/__openerp__.py
@@ -22,7 +22,7 @@
     'name': 'MercadoPago Payment Acquirer',
     'category': 'Hidden',
     'summary': 'Payment Acquirer: MercadoPago Implementation',
-    'version': '9.0.1.0.0',
+    'version': '9.0.1.1.0',
     'author': 'Moldeo Interactive - www.moldeo.coop,ADHOC SA',
     'website': 'www.adhoc.com.ar',
     'license': 'AGPL-3',
---
+++ payment_todopago/migrations/9.0.1.1.0/post-migration.py
@@ -0,0 +1,9 @@
+# -*- coding: utf-8 -*-
+from openupgradelib import openupgrade
+
+
+@openupgrade.migrate()
+def migrate(cr, version):
+    # because this module is renamed, we need to inforce load of this data
+    openupgrade.load_data(
+        cr, 'payment_todopago', 'views/todopago_view.xml')
