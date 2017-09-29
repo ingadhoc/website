b68436bce2fa4d6d9dc547f5daaba20aa7473b4e
Juan Jose Scarafia <jjs@adhoc.com.ar>
2017-09-29 13:22:00 -0300

    IMP website_sale_promotion

--- website_sale_promotion/__openerp__.py
+++ website_sale_promotion/__openerp__.py
@@ -22,7 +22,7 @@
     'name': 'Website Sale Promotion',
     'category': 'Website',
     'summary': 'Website Sale',
-    'version': '9.0.1.0.0',
+    'version': '9.0.1.1.0',
     'author': 'ADHOC SA',
     'website': 'www.adhoc.com.ar',
     'license': 'AGPL-3',
