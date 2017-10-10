62ad0d16ba623f3938ec4c247f35d3dd6ba448dc
Nicolas Mac Rouillon <nicomacr@users.noreply.github.com>
2017-10-10 15:07:32 -0300

    MIG website_sale_clear_line (#46)

--- website_sale_clear_line/static/src/js/website_sale_clear_line.js
+++ website_sale_clear_line/static/src/js/website_sale_clear_line.js
@@ -1,18 +1,21 @@
+odoo.define('website_sale.clear_cart_line', function (require) {
+"use strict";
+    require('website.website');
+    var ajax = require('web.ajax');
+
 $(document).ready(function () {
-$('.oe_website_sale').each(function () {
     var oe_website_sale = this;
     $(oe_website_sale).on("click", ".oe_cart #clear_cart_line_button", function () {
         // obtenemos la linea de la vista
         var $input = $(this);
         var line_id = parseInt($input.data('line-id'),10);
         // pasamos la linea del pedido
-        openerp.jsonRpc("/shop/clear_cart_line", "call", {
+        ajax.jsonRpc("/shop/clear_cart_line", "call", {
                 'line_id': line_id
             }).then(function(){
             location.reload();
         })
         return false;
     })
-
 })
-})
+});
