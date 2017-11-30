b0f9e759088446c85d832b0829fdee6bad671306
nicomacr <nmr@adhoc.com.ar>
2017-11-30 14:01:02 -0300

    9.0 FIX website checout validate document number

--- l10n_ar_website_sale/controllers/main.py
+++ l10n_ar_website_sale/controllers/main.py
@@ -30,12 +30,15 @@ class WebsiteSale(website_sale):
         if not data.get('main_id_category_id') and not config['test_enable']:
             error['main_id_category_id'] = 'missing'
 
+        partner = request.registry.get('res.users').browse(
+            request.cr, SUPERUSER_ID, request.uid, request.context).partner_id
+        number = request.env['res.partner.id_number'].sudo().new({
+            'name': data.get('main_id_number'),
+            'partner_id': partner.id,
+            'category_id': data.get('main_id_category_id'),
+        })
         # validate document number
         try:
-            number = request.env['res.partner.id_category'].new({
-                'name': data.get('main_id_number'),
-                'category_id': data.get('main_id_category_id'),
-            })
             request.env['res.partner.id_category'].sudo().browse(
                 data.get('main_id_category_id')).validate_id_number(
                 number)
@@ -44,6 +47,16 @@ class WebsiteSale(website_sale):
                 'Documento invalido en checkout ecommerce, error: %s' % e)
             error['main_id_number'] = 'error'
             error_message.append(_('Numero de documento invalido'))
+
+        try:
+            number.check()
+        except Exception, e:
+            _logger.info(
+                'Ya existe una empresa con ese número de documento, error:'
+                '%s' % e)
+            error['main_id_number'] = 'error'
+            error_message.append(_(
+                'Ya existe una empresa con ese número de documento'))
 
         # only make state required if there are states on choosen country
         cr, context, pool = (
