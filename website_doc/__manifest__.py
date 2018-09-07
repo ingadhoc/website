90d8522e87e415de34239241b3b5307f727308d2
Juan Jose Scarafia <jjs@adhoc.com.ar>
2018-09-07 16:51:33 -0300

    [ADD] button to child content

--- website_doc/__manifest__.py
+++ website_doc/__manifest__.py
@@ -21,7 +21,7 @@
     'name': 'Website Documentation',
     'category': 'Website',
     'summary': 'Website, Documentation',
-    'version': '11.0.1.0.0',
+    'version': '11.0.1.1.0',
     'author': 'ADHOC SA',
     'website': 'www.adhoc.com.ar',
     'license': 'AGPL-3',
