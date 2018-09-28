fd599c8a330c9945f4cd2f276408f0fc74c118b0
Katherine Zaoral <kz@adhoc.com.ar>
2018-09-28 17:11:11 -0300

    [ADD] l10n_ar_website_sale: Controllers work with AR partner info
    Update old website_sale_taxes_included controllers and use new ones to be able
    to manage and require main_id_category,main_id_number and
    afip_responsabilities of the commercial partner in any website page that shows
    billing address.

--- l10n_ar_website_sale/controllers/main.py
+++ l10n_ar_website_sale/controllers/main.py
@@ -4,78 +4,131 @@
 ##############################################################################
 from odoo.addons.website_sale.controllers.main import WebsiteSale
 from odoo.http import request, route
-# from odoo.tools import config
+from odoo.tools import config
 from odoo import http
 
 
 class L10nArWebsiteSale(WebsiteSale):
 
-    # por ahora no los hacemos obligatorios porque deberian ser solo para el
-    # commercial partner id, TODO encontrarle la vuelta y mejroarlo
-    # def _get_mandatory_billing_fields(self):
-    #     # para no romper los test de odoo
-    #     res = super(L10nArWebsiteSale, self)._get_mandatory_billing_fields()
-    #     if not config['test_enable']:
-    #         res += ["zip"]
-    #     return res + ["main_id_category_id", "main_id_number"]
+    def _get_mandatory_billing_fields(self):
+        # para no romper los test de odoo
+        res = super(L10nArWebsiteSale, self)._get_mandatory_billing_fields()
+        if not config['test_enable']:
+            res += ["zip"]
+        return res + [
+            "main_id_category_id", "main_id_number",
+            "afip_responsability_type_id",
+        ]
+
+    def _checkout_form_save(self, mode, checkout, all_values):
+        post_process = False
+        commercial_partner_id = all_values.get('commercial_partner_id', False)
+        if commercial_partner_id:
+            post_process = True
+            commercial_fields = [
+                'main_id_number',
+                'main_id_category_id',
+                'afip_responsability_type_id',
+            ]
+            values = {}
+            for item in commercial_fields:
+                values.update({
+                    item: checkout.pop(item, all_values.pop(item))
+                })
+
+        res = super(L10nArWebsiteSale, self)._checkout_form_save(
+            mode=mode, checkout=checkout, all_values=all_values)
+
+        if post_process:
+            commercial_partner = request.env['res.partner'].browse(
+                int(commercial_partner_id))
+            commercial_partner.sudo().write(values)
+        return res
 
     @route()
     def address(self, **kw):
         response = super(L10nArWebsiteSale, self).address(**kw)
         document_categories = request.env[
             'res.partner.id_category'].sudo().search([])
-        Partner = request.env['res.partner'].with_context(
-            show_address=1).sudo()
+        afip_responsabilities = request.env[
+            'afip.responsability.type'].sudo().search([])
+        Partner, _website_company = self.get_partner_company()
+        Partner = Partner.with_context(show_address=1).sudo()
         response.qcontext.update({
             'document_categories': document_categories,
-            'invoices':
-                Partner.commercial_partner_id.invoice_ids and True or False,
+            'afip_responsabilities': afip_responsabilities,
+            'partner': Partner,
         })
         return response
 
-    # TODO review all this
-    # NOTA: no lo queremos poner en todos lados porque en realidad en algunos
-    # lugares el precio debe venir sin los impuestos desde la lista de precios
-    # por que los impuestos se agregan después
+    def get_partner_company(self):
+        """ Extract partner info from current user, and company information
+        from website company
+        """
+        uid = request.session.uid or request.env.ref('base.public_user').id
+        partner = request.env['res.users'].browse(uid).partner_id
+        company = request.env['website'].browse(
+            request.context.get('website_id')).company_id
+        return partner, company
 
-    @http.route([
-        '/shop',
-        '/shop/page/<int:page>',
-        '/shop/category/<model("product.public.category"):category>',
-        '/shop/category/<model("product.public.category"):category>/page/'
-        '<int:page>'
-    ], type='http', auth="public", website=True)
-    def shop(self, page=0, category=None, search='', **post):
-        context = request.context
-        context['taxes_included'] = True
-        return super(L10nArWebsiteSale, self).shop(
-            page=page, category=category, search=search, **post)
+    def _get_vat_discriminated(self):
+        vat_discriminated = True
+        partner, company = self.get_partner_company()
+        company_vat_type = company.sale_allow_vat_no_discrimination
+        if company_vat_type and company_vat_type != 'discriminate_default':
+            letters = request.env['account.journal']._get_journal_letter(
+                'sale', company,
+                partner.commercial_partner_id)
+            if letters:
+                vat_discriminated = not letters[0].taxes_included
+            # if no responsability or no letters
+            if not letters and \
+                    company_vat_type == 'no_discriminate_default':
+                vat_discriminated = False
+        return vat_discriminated
 
-    @http.route(
-        ['/shop/product/<model("product.template"):product>'], type='http',
-        auth="public", website=True)
-    def product(self, product, category='', search='', **kwargs):
-        context = request.context
-        context['taxes_included'] = True
-        return super(L10nArWebsiteSale, self).product(
-            product, category=category, search=search, **kwargs)
+    # TODO review
+    # Aca podria ser necesario pasar el afip_responsabilities
+    @route()
+    def checkout(self, **post):
+        _response = super(L10nArWebsiteSale, self).checkout(**post)
+        order = request.website.sale_get_order()
+        redirection = self.checkout_redirection(order)
+        if redirection:
+            return redirection
 
-    @http.route(
-        ['/shop/get_unit_price'], type='json', auth="public", methods=['POST'],
-        website=True)
-    def get_unit_price(
-            self, product_ids, add_qty, use_order_pricelist=False, **kw):
-        context = request.context
-        context['taxes_included'] = True
-        return super(L10nArWebsiteSale, self).get_unit_price(
-            product_ids, add_qty,
-            use_order_pricelist=use_order_pricelist, **kw)
+        if order.partner_id.id == request.website.user_id.sudo().partner_id.id:
+            return request.redirect('/shop/address')
 
-    # for website_sale_options
-    @http.route(
-        ['/shop/modal'], type='json', auth="public", methods=['POST'],
-        website=True)
-    def modal(self, product_id, **kw):
-        context = request.context
-        context['taxes_included'] = True
-        return super(L10nArWebsiteSale, self).modal(product_id, **kw)
+        # --------------------------------------------------------------------
+        # Odoo original code
+        # for f in self._get_mandatory_billing_fields():
+        #     if not order.partner_id[f]:
+        #         return request.redirect('/shop/address?partner_id=%d' %
+        #             order.partner_id.id)
+        # Our code
+        mandatory_billing_fields = self._get_mandatory_billing_fields()
+        commercial_billing_fields = ["main_id_category_id", "main_id_number",
+                                     "afip_responsability_type_id"]
+        for item in commercial_billing_fields:
+            mandatory_billing_fields.pop(mandatory_billing_fields.index(item))
+
+        for f in mandatory_billing_fields:
+            if not order.partner_id[f]:
+                return request.redirect
+                ('/shop/address?partner_id=%d' % order.partner_id.id)
+        for f in commercial_billing_fields:
+            if not order.partner_id.commercial_partner_id[f]:
+                return request.redirect(
+                    '/shop/address?partner_id=%d' % order.partner_id.id)
+        # end
+        # --------------------------------------------------------------------
+
+        values = self.checkout_values(**post)
+
+        values.update({'website_sale_order': order})
+
+        # Avoid useless rendering if called in ajax
+        if post.get('xhr'):
+            return 'ok'
+        return request.render("website_sale.checkout", values)
