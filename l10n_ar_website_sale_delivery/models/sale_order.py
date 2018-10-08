9322ec2a9ec8e7741042e6b89f49a75978b0f2df
Katherine Zaoral <kz@adhoc.com.ar>
2018-10-08 10:43:21 -0300

    [ADD] l10n_ar_website_sale_delivery: remove old amount_delivery_taxed not used.

--- l10n_ar_website_sale_delivery/models/sale_order.py
+++ l10n_ar_website_sale_delivery/models/sale_order.py
@@ -10,22 +10,6 @@ class SaleOrder(models.Model):
 
     _inherit = 'sale.order'
 
-    amount_delivery_taxed = fields.Float(
-        compute='compute_amount_delivery_taxed',
-        string='Delivery Amount With Tax',
-        digits=dp.get_precision('Account'),
-    )
-
-    @api.depends()
-    def compute_amount_delivery_taxed(self):
-        for rec in self:
-            amount_delivery_taxed = 0.0
-            for line in rec.order_line.filtered('is_delivery'):
-                amount_delivery_taxed += line.tax_id.compute_all(
-                    line.price_subtotal, product=line.product_id,
-                    partner=self.partner_id)['total_included']
-            rec.amount_delivery_taxed = amount_delivery_taxed
-
     report_amount_delivery = fields.Float(
         compute='compute_report_amount_delivery',
         string='Delivery Amount',
