d64c8697e77a3ee9cfe83364b61347dd8b1e54a5
Katherine Zaoral <kz@adhoc.com.ar>
2018-10-08 16:02:42 -0300

    [ADD] l10n_ar_website_sale: sale.order.line discount compatible v11.0

--- l10n_ar_website_sale/models/sale_order_line.py
+++ l10n_ar_website_sale/models/sale_order_line.py
@@ -3,33 +3,26 @@
 # directory
 ##############################################################################
 from odoo import models, api, fields
-# from odoo.exceptions import Warning
 import odoo.addons.decimal_precision as dp
-import logging
-_logger = logging.getLogger(__name__)
 
 
 class sale_order_line(models.Model):
+
     _inherit = 'sale.order.line'
 
-    # we overwrite this field to add taxes or not regarding company
-    # configuration
-    discounted_price = fields.Float(
-        compute='_fnct_get_discounted_price',
-        string='Discounted price',
-        digits=dp.get_precision('Product Price')
+    report_discount = fields.Float(
+        compute='compute_report_discount',
+        string='Discount (%)',
+        digits=dp.get_precision('Discount')
     )
 
-    @api.multi
-    def _fnct_get_discounted_price(self):
-        # TODO add company condition to include or not
-        # TODO perhups we can use l10n_ar_sale printed prices
+    @api.depends()
+    def compute_report_discount(self):
         for line in self:
-            discounted_price = (
-                line.price_unit *
+            ret = 'total_excluded' if line.order_id.vat_discriminated \
+                else 'total_included'
+            discount = (line.price_unit *
                 (1.0 - (line.discount or 0.0) / 100.0))
-            # add taxes
-            discounted_price = line.tax_id.compute_all(
-                discounted_price, product=line.product_id,
-                partner=line.order_id.partner_id)['total_included']
-            line.discounted_price = discounted_price
+            line.report_discount = line.tax_id.compute_all(
+                discount, product=line.product_id,
+                partner=line.order_id.partner_id)[ret]
