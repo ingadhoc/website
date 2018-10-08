db4098db21354665a190068ae17f32941f828fd4
Katherine Zaoral <kz@adhoc.com.ar>
2018-10-08 16:30:00 -0300

    [ADD] l10n_ar_website_sale: use commercial partner always

--- l10n_ar_website_sale/models/res_partner.py
+++ l10n_ar_website_sale/models/res_partner.py
@@ -17,17 +17,19 @@ class ResPartner(models.Model):
 
         # Get parter
         user_id = self._context.get('uid', self.env.user.id)
-        partner = self.env['res.users'].browse(user_id).partner_id
+        partner = self.env['res.users'].browse(
+            user_id).partner_id.commercial_partner_id
         return company, partner
 
     @api.model
     def _get_vat_discriminated(self, partner, company):
+        """ parter expected commercial_partner.
+        """
         vat_discriminated = True
         company_vat_type = company.sale_allow_vat_no_discrimination
         if company_vat_type and company_vat_type != 'discriminate_default':
             letters = self.env['account.journal']._get_journal_letter(
-                'sale', company,
-                partner.commercial_partner_id)
+                'sale', company, partner)
             if letters:
                 vat_discriminated = not letters[0].taxes_included
             # if no responsability or no letters
