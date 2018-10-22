c022166924797c6b3063f1eec6914cfa4bf86071
Katherine Zaoral <kz@adhoc.com.ar>
2018-10-22 16:26:03 -0300

    [ADD] l10n_ar_website_sale: set b2c taxes mode by default.

--- l10n_ar_website_sale/wizards/res_config_settings.py
+++ l10n_ar_website_sale/wizards/res_config_settings.py
@@ -2,6 +2,13 @@
 # For copyright and license notices, see __manifest__.py file in module root
 # directory
 ##############################################################################
-from . import controllers
-from . import models
-from . import wizards
+from odoo import models, fields
+
+
+class ResConfigSettings(models.TransientModel):
+
+    _inherit = 'res.config.settings'
+
+    sale_show_tax = fields.Selection(
+        default='total',
+    )
