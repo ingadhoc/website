6f91a7d509a0a1179e57afc597b1ad79618d51c6
Katherine Zaoral <zaoral@users.noreply.github.com>
2018-12-21 17:22:21 -0300

    [ADD] website_sale_ux: Do not show thanks message when cancel or error ocurrs in payment transaction (#97)

--- website_sale_ux/__manifest__.py
+++ website_sale_ux/__manifest__.py
@@ -20,7 +20,7 @@
 {
     'name': 'Website Sale UX',
     'category': 'base.module_category_knowledge_management',
-    'version': '11.0.1.0.0',
+    'version': '11.0.1.1.0',
     'author': 'ADHOC SA',
     'website': 'www.adhoc.com.ar',
     'license': 'AGPL-3',
@@ -32,6 +32,7 @@
     'demo': [],
     'data': [
         'views/product_template_views.xml',
+        'views/templates.xml',
     ],
     'installable': True,
 }
