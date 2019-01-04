eeaa5ba7758098a2fcaa2ee1192dda04909f45d0
Juan Jose Scarafia <jjs@adhoc.com.ar>
2019-01-04 18:19:49 -0300

    Revert "[REM] delete deprecated website_security module. Now similar functionality available on odoo core (#64)"
    This reverts commit 80e933a79e4aead295be884e6eb1517ab73beaf3.

---
+++ website_security/migrations/9.0.1.0.0/post-migration.py
@@ -0,0 +1,29 @@
+##############################################################################
+#
+#    OpenUpgrade module for Odoo
+#    @copyright 2015-Today: Odoo Community Association
+#    @author: Stephane LE CORNEC
+#
+#    This program is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU Affero General Public License as
+#    published by the Free Software Foundation, either version 3 of the
+#    License, or (at your option) any later version.
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU Affero General Public License for more details.
+#
+#    You should have received a copy of the GNU Affero General Public License
+#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#
+##############################################################################
+
+from openupgradelib import openupgrade
+
+
+@openupgrade.migrate()
+def migrate(cr, version):
+    # because this module is renamed, we need to inforce load of this data
+    openupgrade.load_data(
+        cr, 'website_security', 'migrations/9.0.1.0.0/mig_data.xml')
