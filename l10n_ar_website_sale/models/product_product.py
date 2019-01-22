c683bd4d7383c3dd48b6058436bf8dcc514682c4
Katherine Zaoral <kz@adhoc.com.ar>
2019-01-22 17:28:02 -0300

    [FIX] l10n_ar_website_sale: update _website_price method.
    Odoo original code has change in order to fix and error about when to
    show the discount of the product (if there is o not a difference with the
    actual price)
    
    For that we also add it here, and update it in order to maintain the code
    as similar to the orginal as possible making only required changes in order
    to make review differ between original and our changes more easy.

--- l10n_ar_website_sale/models/product_product.py
+++ l10n_ar_website_sale/models/product_product.py
@@ -1,5 +1,11 @@
+##############################################################################
+# For copyright and license notices, see __manifest__.py file in module root
+# directory
+##############################################################################
+# flake8: noqa
+# pylint: disable=pointless-string-statement
 from odoo import models
-from odoo.tools import float_is_zero, pycompat
+from odoo.tools import float_compare, pycompat
 from odoo.http import request
 
 
@@ -7,37 +13,47 @@ class ProductProduct(models.Model):
 
     _inherit = 'product.product'
 
+    # NOTE this a copy of original odoo code that was added and edited here
+    # because it was not able to inherit in other way.
     def _website_price(self):
+        super(ProductProduct, self)._website_price()
+
         qty = self._context.get('quantity', 1.0)
-        # partner = self.env.user.partner_id
+        # ODOO ORIGINAL CODE COMMENTED
+        """
+        partner = self.env.user.partner_id
+        """
+        # THIS IS THE NEW CHANGE
         partner = request.env.user.partner_id.commercial_partner_id
+        # END OF NEW CHANGE
+
         current_website = self.env['website'].get_current_website()
         pricelist = current_website.get_current_pricelist()
         company_id = current_website.company_id
-        context = dict(self._context, pricelist=pricelist.id, partner=partner)
-        self2 = \
-            self.with_context(context) if self._context != context else self
 
-        # Us
+        context = dict(self._context, pricelist=pricelist.id, partner=partner)
+        self2 = self.with_context(context) if self._context != context else self
+
+        # ODOO ORIGINAL CODE COMMENTED
+        """
+        ret = self.env.user.has_group('sale.group_show_price_subtotal') and  'total_excluded' or 'total_included'
+        """
+        # THIS IS THE NEW CHANGE
         # company_id, partner = partner.get_company_partner()
         taxes_included = not partner._get_vat_discriminated(
             partner, company_id)
         ret = 'total_included' if taxes_included else 'total_excluded'
-        # Odoo original code
-        # ret = self.env.user.has_group('sale.group_show_price_subtotal') and
-        # 'total_excluded' or 'total_included'
+        # END OF NEW CHANGE
 
         for p, p2 in pycompat.izip(self, self2):
-            taxes = partner.property_account_position_id.map_tax(
-                p.taxes_id.filtered(
-                    lambda x: x.company_id == company_id))
-            p.website_price = taxes.compute_all(
-                p2.price, pricelist.currency_id, quantity=qty, product=p2,
-                partner=partner)[ret]
-            price_without_pricelist = taxes.compute_all(
-                p.list_price, pricelist.currency_id)[ret]
-            p.website_price_difference = False if float_is_zero(
-                price_without_pricelist - p.website_price,
-                precision_rounding=pricelist.currency_id.rounding) else True
-            p.website_public_price = taxes.compute_all(
-                p2.lst_price, quantity=qty, product=p2, partner=partner)[ret]
+            taxes = partner.property_account_position_id.map_tax(p.sudo().taxes_id.filtered(lambda x: x.company_id == company_id))
+            p.website_price = taxes.compute_all(p2.price, pricelist.currency_id, quantity=qty, product=p2, partner=partner)[ret]
+            # We must convert the price_without_pricelist in the same currency than the
+            # website_price, otherwise the comparison doesn't make sense. Moreover, we show a price
+            # difference only if the website price is lower
+            price_without_pricelist = p.list_price
+            if company_id.currency_id != pricelist.currency_id:
+                price_without_pricelist = company_id.currency_id.compute(price_without_pricelist, pricelist.currency_id)
+            price_without_pricelist = taxes.compute_all(price_without_pricelist, pricelist.currency_id)[ret]
+            p.website_price_difference = True if float_compare(price_without_pricelist, p.website_price, precision_rounding=pricelist.currency_id.rounding) > 0 else False
+            p.website_public_price = taxes.compute_all(p2.lst_price, quantity=qty, product=p2, partner=partner)[ret]
