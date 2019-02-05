dea040834bb32fa9552aa9f1e44291b53c5cde69
Katherine Zaoral <zaoral@users.noreply.github.com>
2019-02-05 18:12:15 -0300

    [FIX] website_product_pack: Show proper price of pack components in Cart (#105)
    * [FIX] website_product_pack: Show proper price of pack components in Cart
    
    Cart does not show discount information, for that reason we now shoe
    price_unit + discount in the price unit of each componennt in the pack
    
    This way make sence the price of each components with the sale order total.
    
    * [ADD] website_product_pack: Be able to remove componentes of a componente
    price pack from cart.

--- website_product_pack/__manifest__.py
+++ website_product_pack/__manifest__.py
@@ -20,7 +20,7 @@
 
 {
     'name': 'Website Product Pack',
-    'version': '11.0.1.1.0',
+    'version': '11.0.1.2.0',
     'category': 'Website',
     'author': 'ADHOC SA, Odoo Community Association (OCA)',
     'website': 'www.adhoc.com.ar',
--- website_product_pack/models/__init__.py
+++ website_product_pack/models/__init__.py
@@ -6,3 +6,4 @@
 from . import product_template
 from . import sale_order
 from . import website
+from . import product_pack_line
