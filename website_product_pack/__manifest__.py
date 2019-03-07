a9f4ae3bc33849fe4ba8c8b2ff217a11928b3ace
Katherine Zaoral <kz@adhoc.com.ar>
2019-03-07 17:35:57 -0300

    [ADD] website_product_pack: new way to use allow_modify_pack functionality

--- website_product_pack/__manifest__.py
+++ website_product_pack/__manifest__.py
@@ -20,7 +20,7 @@
 
 {
     'name': 'Website Product Pack',
-    'version': '11.0.1.2.1',
+    'version': '11.0.1.3.0',
     'category': 'Website',
     'author': 'ADHOC SA, Odoo Community Association (OCA)',
     'website': 'www.adhoc.com.ar',
