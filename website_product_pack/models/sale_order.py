0c65f05b9827521369f52fa32f39cea939bd5ba9
Katherine Zaoral <zaoral@users.noreply.github.com>
2019-03-07 18:04:15 -0300

    [FIX/IMP] product_pack: Allow modify pack new use way and pass context to do not reset sale order after confirm from website (#113)
    * [FIX] website_product_pack: pass context in order to not reset pack's
    components when confirm sale order
    
    * [ADD] website_product_pack: new way to use allow_modify_pack functionality

--- website_product_pack/models/sale_order.py
+++ website_product_pack/models/sale_order.py
@@ -15,7 +15,8 @@ class SaleOrder(models.Model):
             line_id=None, add_qty=0, set_qty=0, **kwargs):
         sale_order_line = self.env['sale.order.line'].browse(line_id)
         if sale_order_line.pack_parent_line_id and not \
-           sale_order_line.pack_parent_line_id.product_id.allow_modify_pack:
+           sale_order_line.pack_parent_line_id.product_id.allow_modify_pack \
+           == 'frontend_backend':
             return {
                 'line_id': line_id,
                 'quantity': sale_order_line.product_uom_qty}
