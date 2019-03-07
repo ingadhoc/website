7ff66ab8495cbd3ac680f9a6f54176081e9d12e7
Katherine Zaoral <kz@adhoc.com.ar>
2019-03-07 17:35:29 -0300

    [FIX] website_product_pack: pass context in order to not reset pack's components when confirm sale order

--- website_product_pack/models/website.py
+++ website_product_pack/models/website.py
@@ -13,7 +13,10 @@ class Website(models.Model):
     @api.multi
     def sale_get_order(self, force_create=False, code=None,
                        update_pricelist=False, force_pricelist=False):
-        self = self.with_context(from_cart=1)
+        self = self.with_context(
+            from_cart=1,
+            update_pricelist=update_pricelist,
+        )
         res = super(Website, self).sale_get_order(
             force_create=force_create, code=code,
             update_pricelist=update_pricelist, force_pricelist=force_pricelist)
