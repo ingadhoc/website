03f09be9827667a37ae39648732d88aa25468bf2
Juan Jose Scarafia <jjs@adhoc.com.ar>
2019-03-11 13:02:42 -0300

    [IMP] add constraint on adding unpublished products to a published pack

--- website_product_pack/models/__init__.py
+++ website_product_pack/models/__init__.py
@@ -4,5 +4,6 @@
 ##############################################################################
 
 from . import product_template
+from . import product_product
 from . import sale_order
 from . import website
