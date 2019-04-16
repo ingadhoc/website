5576220c8a63462c645495dbb1d3671b378a989b
Katherine Zaoral <zaoral@users.noreply.github.com>
2019-04-16 15:48:48 -0300

    [ADD] website_doc: Add ccs class for website doc content in order to define own doc style. (#114)

--- website_doc/__manifest__.py
+++ website_doc/__manifest__.py
@@ -21,7 +21,7 @@
     'name': 'Website Documentation',
     'category': 'Website',
     'summary': 'Website, Documentation',
-    'version': '11.0.1.2.0',
+    'version': '11.0.1.3.0',
     'author': 'ADHOC SA',
     'website': 'www.adhoc.com.ar',
     'license': 'AGPL-3',
