7d1a0fe74de4d9ad4ff814d5f70b33f9c5d66226
Lucas L. Soto <ls@adhoc.com.ar>
2019-08-21 13:14:45 -0300

    [ADD] website_sale_breadcrumb

---
+++ website_sale_breadcrumb/__manifest__.py
@@ -0,0 +1,35 @@
+###############################################################################
+#
+#    Trey, Kilobytes de Soluciones
+#    Copyright (C) 2019-Today Trey, Kilobytes de Soluciones <www.trey.es>
+#
+#    This program is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU Affero General Public License as
+#    published by the Free Software Foundation, either version 3 of the
+#    License, or (at your option) any later version.
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU Affero General Public License for more details.
+#
+#    You should have received a copy of the GNU Affero General Public License
+#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#
+###############################################################################
+{
+    'name': 'Website Sale Breadcrumb',
+    'summary': 'Complete category and product breadcrumb',
+    'category': 'Website',
+    'version': '11.0.1.0.0',
+    'author': 'Trey (www.trey.es)',
+    'website': 'https://www.trey.es',
+    'license': 'AGPL-3',
+    'depends': [
+        'website_sale',
+    ],
+    'data': [
+        'templates/website_sale_breadcrumb.xml',
+    ],
+    'installable': True,
+}
