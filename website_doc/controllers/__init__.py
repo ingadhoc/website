942a4c6a56ffe7b575210b86763cf3c47a85f01b
Nicolás Mac Rouillon <nmr@adhoc.com.ar>
2020-06-05 09:39:42 -0300

    [12.0] [REM] website_doc: move module to a different repository.

--- website_doc/controllers/__init__.py
+++
@@ -1,7 +0,0 @@
-##############################################################################
-# For copyright and license notices, see __manifest__.py file in module root
-# directory
-##############################################################################
-
-from . import main
-from . import website_search
