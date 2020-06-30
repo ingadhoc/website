1336d1ba0446c13bed1f96c94c306a1890e97c4f
Juan Jose Scarafia <jjs@adhoc.com.ar>
2020-06-30 11:40:21 -0300

    [REM] l10n_ar_website_sale/delivery: deprecated We are going to use the PR's to odoo

--- l10n_ar_website_sale/models/__init__.py
+++
@@ -1,8 +0,0 @@
-##############################################################################
-# For copyright and license notices, see __manifest__.py file in module root
-# directory
-##############################################################################
-from . import sale_order_line
-from . import res_partner
-from . import product_template_attribute_value
-from . import product_template
