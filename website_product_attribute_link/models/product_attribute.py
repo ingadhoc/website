68562e3cf3b5e88e45ad25a73f4164d90571070f
nmadhoc <nm@adhoc.com.ar>
2020-07-06 08:15:02 -0300

    [MIG] website_product_attribute_link: Migration to 13.0

--- website_product_attribute_link/models/product_attribute.py
+++ website_product_attribute_link/models/product_attribute.py
@@ -15,3 +15,20 @@ class ProductAttribute(models.Model):
         help='Add a link on website product view so that user can click on an '
         'attribute value and go to a search of products of that value'
     )
+
+
+class ProductTemplateAttributeLine(models.Model):
+    _inherit = 'product.template.attribute.line'
+
+    def _prepare_single_value(self):
+        """On the product page group together the attribute lines that concern
+        the same attribute and that have only one value each.
+
+        Indeed those are considered informative values, they do not generate
+        choice for the user, so they are displayed below the configurator.
+
+        The returned attributes are ordered as they appear in `self`, so based
+        on the order of the attribute lines.
+        """
+        single_value_lines = self.filtered(lambda ptal: len(ptal.value_ids) == 1)
+        return single_value_lines
