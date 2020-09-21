7fc4ddf50bc134038200f304ff63fa1dd5e7abb2
Juan Jose Scarafia <jjs@adhoc.com.ar>
2020-09-21 08:07:28 -0300

    [MIG] website_sale_search_improved/: Migration to 13.0
    X-original-commit: a985969737c722e90fbd784b6999d1eae4411393

--- website_sale_search_improved/controllers/main.py
+++ website_sale_search_improved/controllers/main.py
@@ -4,26 +4,22 @@
 ##############################################################################
 from odoo.addons.website_sale.controllers.main import WebsiteSale
 from odoo.http import request
+from odoo.osv import expression
 
 
-def new_get_search_domain(self, search, category, attrib_values):
+def new_get_search_domain(self, search, category, attrib_values, search_in_description=True):
     """ Monkey patch instead of overwriting method to allow others inheriting
     this method
     """
     # TODO ver si usamos modulo de la oca de buscar por atributos
     # tambien pdoemos ver si cambiamos logica de que filtre como hace
     # vauxoo cambiando la parte de attrib_values comparando con or
-    domain = request.website.sale_product_domain()
-    # inicio cambio
+    domains = [request.website.sale_product_domain()]
     if search:
-        domain += request.env['product.template']._search_smart_search(
-            'ilike', search)
-        if 'brand_id' in request.env.context:
-            domain.append(
-                ('product_brand_id', '=', request.env.context['brand_id']))
+        domains.append(request.env['product.template']._search_smart_search('ilike', search))
 
     if category:
-        domain += [('public_categ_ids', 'child_of', int(category))]
+        domains.append([('public_categ_ids', 'child_of', int(category))])
 
     if attrib_values:
         attrib = None
@@ -35,12 +31,13 @@ def new_get_search_domain(self, search, category, attrib_values):
             elif value[0] == attrib:
                 ids.append(value[1])
             else:
-                domain += [('attribute_line_ids.value_ids', 'in', ids)]
+                domains.append([('attribute_line_ids.value_ids', 'in', ids)])
                 attrib = value[0]
                 ids = [value[1]]
         if attrib:
-            domain += [('attribute_line_ids.value_ids', 'in', ids)]
-    return domain
+            domains.append([('attribute_line_ids.value_ids', 'in', ids)])
+
+    return expression.AND(domains)
 
 
 WebsiteSale._get_search_domain = new_get_search_domain
