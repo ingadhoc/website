2237a3a7965adaa2a20fb6f238cbecc53e9776bf
Nicolás Mac Rouillon <nmr@adhoc.com.ar>
2020-10-23 10:01:46 -0300

    [13.0] [FIX] website_product_pack: We remove the lines related to a pack product when this product is removed from the order.
    Ticket 32369

--- website_product_pack/models/sale_order.py
+++ website_product_pack/models/sale_order.py
@@ -18,6 +18,9 @@ class SaleOrder(models.Model):
             return {
                 'line_id': line_id,
                 'quantity': sale_order_line.product_uom_qty}
+        # we force to remove the packs lines when the partent line is removed from the order.
+        if set_qty == 0 and sale_order_line.pack_child_line_ids:
+            sale_order_line.pack_child_line_ids.unlink()
         return super()._cart_update(
             product_id=product_id, line_id=line_id,
             add_qty=add_qty, set_qty=set_qty, **kwargs)
