4ed965bc7f56ad25efcd15af17ccee8ef97cf45e
Nicolás Mac Rouillon <nmr@adhoc.com.ar>
2021-02-03 17:26:49 -0300

    [13.0] [FIX] website_product_attribute_link: Set the value of the attribute product to get ID.

--- website_product_attribute_link/__manifest__.py
+++ website_product_attribute_link/__manifest__.py
@@ -19,7 +19,7 @@
 ##############################################################################
 {
     'name': 'Product Attribute Link on Website Product view',
-    'version': '13.0.1.0.0',
+    'version': '13.0.1.1.0',
     'category': 'Product',
     'sequence': 14,
     'summary': '',
