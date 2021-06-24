ffcfaed68662a8616553d5bf8c2a736b4aa75dd4
Juan Jose Scarafia <jjs@adhoc.com.ar>
2021-06-24 07:51:49 -0300

    [REM] payment_mercadopago: deprecated

--- payment_mercadopago/__init__.py
+++
@@ -1,7 +0,0 @@
-##############################################################################
-# For copyright and license notices, see __manifest__.py file in module root
-# directory
-##############################################################################
-from . import models
-from . import controllers
-from odoo.addons.payment.models.payment_acquirer import create_missing_journal_for_acquirers
