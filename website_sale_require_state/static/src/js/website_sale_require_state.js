288c329c2675a1be204d28c309dd2f5213cc56d4
Juan Ignacio Rivero <jr@adhoc.com.ar>
2021-11-18 11:41:13 -0300

    [IMP]website_sale_require_state: Require State
    Require state selection and zip code on address management on checkout

---
+++ website_sale_require_state/static/src/js/website_sale_require_state.js
@@ -0,0 +1,66 @@
+odoo.define('website_sale_require_state.requireState', function (require) {
+'use strict';
+
+
+var publicWidget = require('web.public.widget');
+require('website_sale.website_sale');
+
+
+
+publicWidget.registry.WebsiteSale.include({
+
+    /**
+    * @override
+    * @private
+    */
+    _changeCountry: function () {
+        if (!$("#country_id").val()) {
+            return;
+        }
+        this._rpc({
+            route: "/shop/country_infos/" + $("#country_id").val(),
+            params: {
+                mode: 'shipping',
+            },
+        }).then(function (data) {
+            // placeholder phone_code
+            //$("input[name='phone']").attr('placeholder', data.phone_code !== 0 ? '+'+ data.phone_code : '');
+
+            // populate states and display
+            var selectStates = $("select[name='state_id']");
+            // dont reload state at first loading (done in qweb)
+            if (selectStates.data('init') === 0 || selectStates.find('option').length === 1) {
+                if (data.states.length) {
+                    selectStates.html('');
+                    selectStates.append('<option value="">State / Province...</option>');
+                    _.each(data.states, function (x) {
+                        var opt = $('<option>').text(x[1])
+                            .attr('value', x[0])
+                            .attr('data-code', x[2]);
+                        selectStates.append(opt);
+                    });
+                    selectStates.parent('div').show();
+                } else {
+                    selectStates.val('').parent('div').hide();
+                }
+                selectStates.data('init', 0);
+            } else {
+                selectStates.data('init', 0);
+            }
+
+            // manage fields order / visibility
+            if (data.fields) {
+                if ($.inArray('zip', data.fields) > $.inArray('city', data.fields)) {
+                    $(".div_zip").before($(".div_city"));
+                } else {
+                    $(".div_zip").after($(".div_city"));
+                }
+                var all_fields = ["street", "zip", "city", "country_name"]; // "state_code"];
+                _.each(all_fields, function (field) {
+                    $(".checkout_autoformat .div_" + field.split('_')[0]).toggle($.inArray(field, data.fields) >= 0);
+                });
+            }
+        });
+    },
+})
+});
