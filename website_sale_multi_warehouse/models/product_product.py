f789376097ce234b305433eff2aba68ec5a7a5b0
Juan Ignacio Rivero <jr@adhoc.com.ar>
2022-02-25 20:58:52 +0000

    [13.0][FIX]website_sale_stock_ux: multiple published warehouses
    closes ingadhoc/website#224
    
    X-original-commit: 0bbb124a4d3e16542182e07bb6ae84e6216ee94f
    Signed-off-by: Juan José Scarafía <jjs@adhoc.com.ar>

--- website_sale_multi_warehouse/models/product_product.py
+++ website_sale_multi_warehouse/models/product_product.py
@@ -9,13 +9,15 @@ class ProductProduct(models.Model):
         """ If we are on website and there isn't a warehouse or location on
         the context, we get only published warehouses
         """
-        if not self._context.get('website_id') or \
-           self._context.get('warehouse') or \
-           self._context.get('location'):
+        if self._context.get('website_id') and self._context.get('warehouse'):
+            # _get_domain_locations no admite por contexto ninguna clave para evaluar stock en varios almacenes /
+            # ubicaciones, entonces hacemos la busqueda nosotros y luego llamamos directamente a _get_domain_locations_ne
+            company_id = self.env.context.get('force_company', self.env.company.id)
+            location_ids = self.env['stock.warehouse'].search(
+                [('website_published', '=', True), ('company_id', '=', company_id)]).mapped('view_location_id').ids
+            return self._get_domain_locations_new(
+                location_ids,
+                company_id=self.env.context.get('force_company', False),
+                compute_child=self.env.context.get('compute_child', True))
+        else:
             return super()._get_domain_locations()
-        location_ids = self.env['stock.warehouse'].search(
-            [('website_published', '=', True)]).mapped('view_location_id').ids
-        return self._get_domain_locations_new(
-            location_ids,
-            company_id=self.env.context.get('force_company', False),
-            compute_child=self.env.context.get('compute_child', True))
