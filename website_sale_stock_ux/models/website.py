51aee4dda12efc3974332fc9a8707af424481f51
Juan Ignacio Rivero <jr@adhoc.com.ar>
2022-06-03 16:08:53 -0300

    [FIX]website_sale_stock_ux: draft work on local

--- website_sale_stock_ux/models/website.py
+++ website_sale_stock_ux/models/website.py
@@ -6,6 +6,7 @@ class Website(models.Model):
     _inherit = 'website'
 
     def _get_warehouse_available(self):
+        # import pdb; pdb.set_trace()
         # this method is called to get stock and it's overriding by this module on _get_domain_locations so that
         # it get stock for all published wareouse, but it's also used to set warehouse on SO
         # for this second use case we only return the warehouse if it's published
