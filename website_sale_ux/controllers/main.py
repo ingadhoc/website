6cf8e357309a1b9d3765bd3f9e4faa06ccb7a50e
Juan Jose Scarafia <jjs@adhoc.com.ar>
2023-05-24 17:27:33 -0300

    [REM] website_sale_ux: search custom behaviour En16 nos quedo un hibrido de cosas que no es para lo cual había sido diseñada la funcionalidad. En el pasado habíamos toquetado cosas para lograr este comportamiento: a) al buscar por algo se elimina filtro de categorías b) al hacer click en "todos los productos" se elimina por que se estaba filtrando
    Como esta hoy "b" no funciona y además se termina rompiendo algo adicional, que es que si uno buscó por algo y cambia las categorías, ese cambio no funciona y queda muy raro.
    
    Volvemos a comportamiento nativo de odoo donde la busqueda aplica sobre el filtro de las categorías y cambiar de categorías NO resetea filtro de búsqueda. Renombramos "all products" por "all categories" para que sea mas representativo con este comportamiento.

--- website_sale_ux/controllers/main.py
+++ website_sale_ux/controllers/main.py
@@ -2,23 +2,8 @@
 # For copyright and license notices, see __manifest__.py file in module root
 # directory
 ##############################################################################
-from odoo.addons.website_sale.controllers.main import WebsiteSale
 from odoo.addons.sale_product_configurator.controllers.main import ProductConfiguratorController
-from odoo import http
 from odoo.http import request
-
-
-class WebsiteSaleCustom(WebsiteSale):
-
-    @http.route()
-    # TODO make optional
-    def shop(self, page=0, category=None, search='', ppg=False, **post):
-        """
-        If we have a search and category, clean category
-        """
-        if category and search:
-            category = None
-        return super().shop(page=page, category=category, search=search, ppg=ppg, **post)
 
 
 class ProductConfiguratorControllerCustom(ProductConfiguratorController):
