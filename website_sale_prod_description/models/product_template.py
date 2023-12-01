9291b1d6c85f2e8d005eed5dfdf64db9c11f2172
Franco Leyes <lef@adhoc.com.ar>
2023-12-01 13:27:09 -0300

    [IMP] website_sale_prod_description: add functionality for it to also search in the 'description_website' field in the website search
    X-original-commit: bd74bd0df114ef89f6a060175b5f953c6627f33c

--- website_sale_prod_description/models/product_template.py
+++ website_sale_prod_description/models/product_template.py
@@ -2,7 +2,7 @@
 # For copyright and license notices, see __manifest__.py file in module root
 # directory
 ##############################################################################
-from odoo import fields, models
+from odoo import fields, models, api
 
 
 class ProductTemplate(models.Model):
@@ -15,3 +15,11 @@ class ProductTemplate(models.Model):
         " ecommerce to your customers. This description will not be used on "
         "Sales Orders, Deliveries and Invoices"
     )
+
+    @api.model
+    def _search_get_detail(self, website, order, options):
+        result = super(ProductTemplate, self)._search_get_detail(website, order, options)
+        if options.get('displayDescription'):
+            result['search_fields'].append('description_website')
+            result['fetch_fields'].append('description_website')
+        return result
