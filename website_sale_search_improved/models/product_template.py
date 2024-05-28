1ab6443dc862285d17a4604cedc946bd7db0a173
Bruno Zanotti <bz@adhoc.com.ar>
2024-05-28 19:58:25 +0000

    [FIX] website_sale_search_improved: portal users Access Error
    closes ingadhoc/website#286
    
    Signed-off-by: matiasperalta1 <mnp@adhoc.com.ar>

--- website_sale_search_improved/models/product_template.py
+++ website_sale_search_improved/models/product_template.py
@@ -1,4 +1,6 @@
 from odoo import api, models
+from ast import literal_eval
+
 
 class ProductTemplate(models.Model):
     _inherit = 'product.template'
@@ -8,8 +10,8 @@ class ProductTemplate(models.Model):
         """ Enhance the search functionality in the e-commerce module by adding extra fields for the search.
             Odoo will attempt to match the search term exactly within these additional fields.
         """
-
         res = super()._search_get_detail(website, order, options)
-        smart_search_fields = self.sudo().env.ref('product.model_product_template').name_search_ids
-        res['search_fields'] = res['search_fields'] + smart_search_fields.mapped('name')
+        if self.env['ir.config_parameter'].sudo().get_param('website_sale_search_improved.extend_search_fields'):
+            extra_search_fields = literal_eval(self.env['ir.config_parameter'].sudo().get_param('website_sale_search_improved.search_fields'))
+            res['search_fields'] = res['search_fields'] + extra_search_fields
         return res
