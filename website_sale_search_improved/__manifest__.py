8d19efb92262e0512069235503cbeb82d72ca327
Matias Peralta <matias_peralta1@hotmail.com>
2024-07-03 13:49:27 +0000

    [REF] website_sale_search_improved: change string
    Part-of: ingadhoc/website#292

--- website_sale_search_improved/__manifest__.py
+++ website_sale_search_improved/__manifest__.py
@@ -20,7 +20,7 @@
 {
     'name': 'Website Sale Search Improved',
     'category': 'Hidden',
-    'version': "16.0.1.0.0",
+    'version': "16.0.1.1.0",
     'author': 'ADHOC SA',
     'website': 'www.adhoc.com.ar',
     'license': 'AGPL-3',
