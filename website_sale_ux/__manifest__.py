497047cfbff5d45c0be3eef5b8c8c7e787264822
“Juan <jr@somosadhoc.com>
2024-09-17 18:03:59 +0000

    [IMP]website_sale_ux: Out of stock ribbon
    closes ingadhoc/website#317
    
    Signed-off-by: lav-adhoc <lav@adhoc.com.ar>

--- website_sale_ux/__manifest__.py
+++ website_sale_ux/__manifest__.py
@@ -20,7 +20,7 @@
 {
     'name': 'Website Sale UX',
     'category': 'base.module_category_knowledge_management',
-    'version': "17.0.1.3.0",
+    'version': "17.0.1.4.0",
     'author': 'ADHOC SA',
     'website': 'www.adhoc.com.ar',
     'license': 'AGPL-3',
