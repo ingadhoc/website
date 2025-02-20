56d374973da2462d43c9a623a782f309a8519ba6
Alexis Lopez <loa@adhoc.com.ar>
2025-02-20 16:22:43 -0300

    [FIX] website_sale_ux: remove ribbon

--- website_sale_ux/__manifest__.py
+++ website_sale_ux/__manifest__.py
@@ -20,7 +20,7 @@
 {
     'name': 'Website Sale UX',
     'category': 'base.module_category_knowledge_management',
-    'version': "17.0.1.4.0",
+    'version': "17.0.1.5.0",
     'author': 'ADHOC SA',
     'website': 'www.adhoc.com.ar',
     'license': 'AGPL-3',
