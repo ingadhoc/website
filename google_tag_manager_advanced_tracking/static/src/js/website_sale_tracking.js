65213e63bf112a14a7c2464d4942391b1a808c87
“Juan <jr@adhoc.com.ar>
2025-03-07 12:28:59 -0300

    [IMP]google_tag_manager_advanced_tracking: updates

--- google_tag_manager_advanced_tracking/static/src/js/website_sale_tracking.js
+++ google_tag_manager_advanced_tracking/static/src/js/website_sale_tracking.js
@@ -23,43 +23,58 @@ odoo.define("google_tag_manager_advanced_tracking.tracking", function (require) 
             var dataTarget = ev.target.closest('a#add_to_cart');
             var product_id = dataTarget.dataset.product_id;
             var product_name = dataTarget.dataset.product_name;
+            var currency = dataTarget.dataset.currency;
             var product_price = dataTarget.dataset.product_price;
             var product_amount = $("[name=add_qty]").val();
             var amount = parseFloat(product_price * product_amount).toFixed(2);
             const dict = {
                 'event': 'add_to_cart',
-                'value': amount,
-                'items': [{
-                    'item_name': product_name,
-                    'item_id': product_id,
-                    'price': product_price
-                }]
+                'ecommerce': {
+                    'currency': currency,
+                    'value': amount,
+                    'items': [{
+                        'item_name': product_name,
+                        'item_id': product_id,
+                        'price': product_price,
+                        'quantity': product_amount,
+                    }]
+                }
             }
             this._pushInfo(dict);
         },
         _onClickAddToCartProductsItem: function(ev) {
             var dataTarget = ev.target.closest('div.o_wsale_product_btn');
             var product_id = dataTarget.dataset.product_id;
+            var currency = dataTarget.dataset.currency;
             var product_sku = dataTarget.dataset.product_sku;
             var product_name = dataTarget.dataset.product_name;
             var product_price = dataTarget.dataset.product_price;
             const dict = {
                 'event': 'add_to_cart',
-                'value': product_price,
-                'items': {
-                    'item_name': product_name,
-                    'item_id': product_sku || product_id,
-                    'price': product_price
+                'ecommerce': {
+                    'value': product_price,
+                    'currency': currency,
+                    'items': {
+                        'item_name': product_name,
+                        'item_id': product_sku || product_id,
+                        'price': product_price,
+                    }
                 }
             }
             this._pushInfo(dict);
         },
         _onCheckoutStartJs: function () {
             var dataTarget = $("#cart_products")[0];
+            var currency = dataTarget.dataset.currency;
+            var value = dataTarget.dataset.value;
             const info = dataTarget.dataset.cart_info;
             const dict = {
                 'event':'begin_checkout',
-                'ecommerce':{'items':info}
+                'ecommerce':{
+                    'currency': currency,
+                    'value': value,
+                    'items':info
+                }
             }
             this._pushInfo(dict);
         },
