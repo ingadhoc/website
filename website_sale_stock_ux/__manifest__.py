476e1d0f9780089fcb07c9951a478e04d0382aba
Manuela Cepeda <cem@adhoc.com.ar>
2025-05-12 14:24:01 +0000

    [ADD]website_sale_stock_ux:Backport from 18.0
    X-original-commit: abc7fc3309733e10a8196e33b74586ad23ca7f0e

--- website_sale_stock_ux/__manifest__.py
+++ website_sale_stock_ux/__manifest__.py
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 ##############################################################################
 #
 #    Copyright (C) 2015  ADHOC SA  (http://www.adhoc.com.ar)
@@ -35,3 +36,43 @@
     "auto_install": True,
     "application": False,
 }
+||||||| parent of 426299f (temp)
+=======
+##############################################################################
+#
+#    Copyright (C) 2015  ADHOC SA  (http://www.adhoc.com.ar)
+#    All Rights Reserved.
+#
+#    This program is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU Affero General Public License as
+#    published by the Free Software Foundation, either version 3 of the
+#    License, or (at your option) any later version.
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU Affero General Public License for more details.
+#
+#    You should have received a copy of the GNU Affero General Public License
+#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#
+##############################################################################
+{
+    "name": "Webstie Sale Stock UX",
+    "category": "base.module_category_knowledge_management",
+    "version": "17.0.1.0.0",
+    "author": "ADHOC SA",
+    "website": "www.adhoc.com.ar",
+    "license": "AGPL-3",
+    "depends": [
+        "website_sale_stock",
+    ],
+    "data": [
+        "views/templates.xml",
+        "views/snippets.xml",
+    ],
+    "installable": True,
+    "auto_install": True,
+    "application": False,
+}
+>>>>>>> 426299f (temp)
