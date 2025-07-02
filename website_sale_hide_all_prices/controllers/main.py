f69031ca60aba5515689b63fa64d9a7af18bf309
augusto-weiss <awe@adhoc.com.ar>
2025-07-02 14:11:34 +0000

    [FIX] website_sale_hide_all_prices: restructure submit label button
    closes ingadhoc/website#386
    
    Signed-off-by: Manuela Cepeda <cem@adhoc.com.ar>

--- website_sale_hide_all_prices/controllers/main.py
+++ website_sale_hide_all_prices/controllers/main.py
@@ -1,6 +1,7 @@
 from odoo import http
 from odoo.addons.website_sale.controllers.main import WebsiteSale
 from odoo.http import request
+from odoo.tools.translate import _
 
 
 class WebsiteSale(WebsiteSale):
@@ -14,3 +15,9 @@ class WebsiteSale(WebsiteSale):
                 return request.redirect("/request-quotation")
 
         return super().shop_payment_confirmation(**post)
+
+    def _get_shop_payment_values(self, order, **kwargs):
+        payment_values = super()._get_shop_payment_values(order=order, **kwargs)
+        if order and order.website_id.website_hide_all_prices:
+            payment_values["submit_button_label"] = _("Request Quotation")
+        return payment_values
