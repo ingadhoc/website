5535009256d53748eac3755a36af324b680bbb32
“Juan <jr@adhoc.com.ar>
2025-07-28 15:36:55 -0300

    [18.0][MIG]website_sale_installment_plans

---
+++ website_sale_installment_plans/models/account_card_installment.py
@@ -0,0 +1,30 @@
+from odoo import fields, models
+
+
+class AccountCardInstallment(models.Model):
+    _name = "account.card.installment"
+    _inherit = ["account.card.installment", "website.published.mixin"]
+
+    message = fields.Char(help="Mensaje que se muestra al final de la leyenda de cuotas")
+
+    def map_installment_values(self, amount_total):
+        self.ensure_one()
+
+        result = super().map_installment_values(amount_total)
+
+        divisor = self.divisor if self.divisor else 1
+        installment = self.installment if self.installment else 1
+        amount = result["amount"]
+        description = (
+            "En %s cuota de $%.2f" % (divisor, amount)
+            if divisor == 1
+            else "En %s cuotas de $%.2f (Total $%.2f)" % (installment, amount / installment, amount)
+        )
+
+        result.update(
+            {
+                "description": description,
+            }
+        )
+
+        return result
---
+++ website_sale_installment_plans/models/product_template.py
@@ -0,0 +1,43 @@
+from odoo import models
+
+
+class ProductTemplate(models.Model):
+    _inherit = "product.template"
+
+    def _get_published_installments(self):
+        return self.env["account.card.installment"].sudo().search([("is_published", "=", True)])
+
+    def _get_installment_plans(self, price):
+        plans = []
+        for cuota in self._get_published_installments():
+            values = cuota.map_installment_values(price)
+            plans.append(
+                {
+                    "installments": cuota.installment,
+                    "description": values["description"],
+                    "message": cuota.message or "",
+                }
+            )
+        return plans
+
+    def _get_combination_info(
+        self, combination=False, product_id=False, add_qty=1, parent_combination=False, only_template=False
+    ):
+        combination_info = super()._get_combination_info(
+            combination=combination,
+            product_id=product_id,
+            add_qty=add_qty,
+            parent_combination=parent_combination,
+            only_template=only_template,
+        )
+
+        product_price = combination_info.get("price", 0)
+        installment_plans = self._get_installment_plans(product_price)
+        combination_info.update({"installment_plans": installment_plans})
+
+        return combination_info
+
+    def _get_card_installments_for_shop(self, context_price):
+        installment_plans = self._get_installment_plans(context_price)
+
+        return installment_plans
