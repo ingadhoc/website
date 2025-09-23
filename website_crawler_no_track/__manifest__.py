a4d5c20eb63f537e0851eebc1c30fd236b6bfb3a
Martin Quinteros <maq@adhoc.com.ar>
2025-09-23 09:33:45 -0300

    [ADD] new Module website_crawler_no_track
    This module prevents tracking information from being added for requests with user
    agents matching specified crawler names. Its purpose is to avoid
    unnecessary resource consumption caused by intensive requests from web crawlers.
    
    closes ingadhoc/website#408
    
    Signed-off-by: Diego Bollini <dib@adhoc.com.ar>

---
+++ website_crawler_no_track/__manifest__.py
@@ -0,0 +1,17 @@
+{
+    "name": "Website Crawler No Track",
+    "version": "18.0.1.0.0",
+    "category": "Website",
+    "sequence": 14,
+    "summary": "",
+    "author": "ADHOC SA",
+    "website": "www.adhoc.com.ar",
+    "license": "AGPL-3",
+    "images": [],
+    "depends": [
+        "website",
+    ],
+    "data": [],
+    "installable": True,
+    "application": False,
+}
