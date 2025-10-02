559610ab99da736a9daa44365dfc224824a7ded2
migration-bot-adhoc <migration-bot-adhoc@users.noreply.github.com>
2025-10-02 16:21:31 +0000

    [MIG] website_sale_force_confirmation: Migration to 19.0

--- website_sale_force_confirmation/__manifest__.py
+++ website_sale_force_confirmation/__manifest__.py
@@ -20,11 +20,11 @@
 {
     "name": "Website Sale Force Confirmation",
     "category": "Hidden",
-    "version": "18.0.1.0.0",
+    "version": "19.0.1.0.0",
     "author": "ADHOC SA",
     "website": "www.adhoc.com.ar",
     "license": "AGPL-3",
     "depends": ["website_sale"],
     "data": ["views/res_config_settings_views.xml"],
-    "installable": False,
+    "installable": True,
 }
