1dd78e89ae6c8a54be50d570676cc50f8530c8fc
migration-bot-adhoc <migration-bot-adhoc@users.noreply.github.com>
2025-10-02 16:08:38 +0000

    [MIG] website_sale_installment_plans: Migration to 19.0
    closes ingadhoc/website#414
    
    Signed-off-by: Augusto Weiss <awe@adhoc.com.ar>

--- website_sale_installment_plans/models/account_card_installment.py
+++ website_sale_installment_plans/models/account_card_installment.py
@@ -5,7 +5,7 @@ class AccountCardInstallment(models.Model):
     _name = "account.card.installment"
     _inherit = ["account.card.installment", "website.published.mixin"]
 
-    message = fields.Char(help="Mensaje que se muestra al final de la leyenda de cuotas")
+    message = fields.Char(help="Message displayed at the end of the installment legend")
 
     def map_installment_values(self, amount_total):
         self.ensure_one()
@@ -16,9 +16,9 @@ class AccountCardInstallment(models.Model):
         installment = self.installment if self.installment else 1
         amount = result["amount"]
         description = (
-            "En %s cuota de $%.2f" % (divisor, amount)
+            "%s installments of $%.2f" % (divisor, amount)
             if divisor == 1
-            else "En %s cuotas de $%.2f (Total $%.2f)" % (installment, amount / installment, amount)
+            else "%s installments of $%.2f (Total $%.2f)" % (installment, amount / installment, amount)
         )
 
         result.update(
