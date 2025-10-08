01e2b7ee573ff723384410538e1152242bf276f0
migration-bot-adhoc <migration-bot-adhoc@users.noreply.github.com>
2025-10-08 16:44:58 +0000

    [MIG] website_sale_search_improved: Migration to 19.0
    closes ingadhoc/website#421
    
    Signed-off-by: Augusto Weiss <awe@adhoc.com.ar>

--- website_sale_search_improved/static/tests/tours/website_sale_search_improved.js
+++ website_sale_search_improved/static/tests/tours/website_sale_search_improved.js
@@ -1,6 +1,6 @@
 /** @odoo-module **/
 
-import {registry} from "@web/core/registry";
+import { registry } from "@web/core/registry";
 import * as tourUtils from "@website_sale/js/tours/tour_utils";
 
 registry.category("web_tour.tours").add("website_sale_search_improved_tour", {
@@ -9,12 +9,12 @@ registry.category("web_tour.tours").add("website_sale_search_improved_tour", {
          ...tourUtils.searchProduct("Test Description"),
          {
             content: "Verify the selected product is correct.",
-            trigger: 'a[data-oe-expression="product.name"]:contains("Test Product")',
+            trigger: '.oe_product_cart a:contains("Test Product")',
         },
          ...tourUtils.searchProduct("Test Tag"),
          {
             content: "Verify the selected product is correct.",
-            trigger: 'a[data-oe-expression="product.name"]:contains("Test Product")',
+            trigger: '.oe_product_cart a:contains("Test Product")',
         }
     ]
 });
