e765e3b84e9c7d62dbab6025b168ce34a3ea4c9d
migration-bot-adhoc <migration-bot-adhoc@users.noreply.github.com>
2025-10-09 13:00:02 +0000

    [MIG] website_sale_advanced_tracking: Migration to 19.0

--- website_sale_advanced_tracking/static/src/add_to_cart_notification.js
+++ website_sale_advanced_tracking/static/src/add_to_cart_notification.js
@@ -1,8 +1,8 @@
 /** @odoo-module **/
 
-import {AddToCartNotification} from  "@website_sale/js/notification/add_to_cart_notification/add_to_cart_notification";
-import {CartNotification} from  "@website_sale/js/notification/cart_notification/cart_notification";
-import {WarningNotification} from  "@website_sale/js/notification/warning_notification/warning_notification";
+import { AddToCartNotification } from "@website_sale/js/notification/add_to_cart_notification/add_to_cart_notification";
+import { CartNotification } from "@website_sale/js/notification/cart_notification/cart_notification";
+import { WarningNotification } from "@website_sale/js/notification/warning_notification/warning_notification";
 
 class TrackingAddToCartNotification extends AddToCartNotification {
     setup(){
