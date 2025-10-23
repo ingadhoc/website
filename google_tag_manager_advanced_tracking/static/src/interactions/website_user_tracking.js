fe232e3582bbe7250f4abc22a60f3024728316ef
migration-bot-adhoc <migration-bot-adhoc@users.noreply.github.com>
2025-10-23 17:01:02 +0000

    [MIG] google_tag_manager_advanced_tracking: Migration to 19.0
    closes ingadhoc/website#426
    
    Signed-off-by: Manuela Cepeda <cem@adhoc.com.ar>

---
+++ google_tag_manager_advanced_tracking/static/src/interactions/website_user_tracking.js
@@ -0,0 +1,30 @@
+/** @odoo-module **/
+import { registry } from '@web/core/registry';
+import { Interaction } from '@web/public/interaction';
+
+export class GoogleTagManagerAdvancedTrackingUser extends Interaction {
+    static selector = ".oe_website_login_container";
+
+    dynamicContent = {
+        'button.on_user_signup': { 't-on-click': this.onUserSignup },
+    };
+
+    _pushInfo(dict){
+        if (typeof (dataLayer) !== 'undefined') {
+            dataLayer.push(dict);
+            console.log(dict);
+            debugger;
+        }
+    }
+    onUserSignup() {
+        debugger;
+        const user_email = document.getElementById("login").value;
+        const dict = {
+            'event':'user_signup',
+            'user_email':user_email,
+        }
+        this._pushInfo(dict);
+    }
+}
+
+registry.category('public.interactions').add('google_tag_manager_advanced_tracking.GoogleTagManagerAdvancedTrackingUser', GoogleTagManagerAdvancedTrackingUser);
