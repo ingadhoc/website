751d966ef4fbb61212471d0a62e07b8c737cbdef
“Juan <jr@adhoc.com.ar>
2025-11-04 18:52:49 +0000

    [BACKPORT]website_sale_installment_plans to 17.0
    X-original-commit: 2ec31bf57cdd5bc9e57994ea15c05030312d0a8f

--- website_sale_installment_plans/__manifest__.py
+++ website_sale_installment_plans/__manifest__.py
@@ -1,3 +1,4 @@
+<<<<<<< 258b45b0798d4d7fb26bc4b4394bdc5514b27a41
 {
     "name": "Website Sale Installment Plan",
     "category": "Website/Website",
@@ -16,3 +17,24 @@
     ],
     "installable": True,
 }
+||||||| fcf872691914acfba6a243203fc2e50621123288
+=======
+{
+    "name": "Website Sale Installment Plan",
+    "category": "Website/Website",
+    "version": "17.0.1.0.0",
+    "author": "ADHOC SA",
+    "website": "www.adhoc.com.ar",
+    "license": "AGPL-3",
+    "depends": [
+        "product",
+        "website_sale",
+        "card_installment",
+    ],
+    "data": [
+        "views/account_card_installment_views.xml",
+        "views/website_templates.xml",
+    ],
+    "installable": True,
+}
+>>>>>>> c5b91d2928af8bd4ec993630cae91d90362a3ae2
