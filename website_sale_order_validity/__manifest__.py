d2d62805728faba8355f3886810e51ecb0bda8a0
“Juan <jr@adhoc.com.ar>
2026-01-07 13:03:25 -0300

    [IMP]website_sale_order_validity: show button when validity is not set
    closes ingadhoc/website#440
    
    Signed-off-by: Manuela Cepeda <cem@adhoc.com.ar>

--- website_sale_order_validity/__manifest__.py
+++ website_sale_order_validity/__manifest__.py
@@ -20,7 +20,7 @@
 {
     "name": "Website Sale Order Validity",
     "category": "Hidden",
-    "version": "18.0.1.0.0",
+    "version": "18.0.1.1.0",
     "author": "ADHOC SA",
     "website": "www.adhoc.com.ar",
     "license": "AGPL-3",
