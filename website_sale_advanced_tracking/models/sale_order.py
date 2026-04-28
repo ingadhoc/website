a7368a6177adac3cb86fe902a99d16031ea93d2b
“Juan <jr@adhoc.com.ar>
2026-04-28 16:21:06 +0000

    [IMP] google_tag_manager_advanced_tracking: shipping and discount separated from items / fix facebook pixel tracking #115773
    closes ingadhoc/website#454
    
    X-original-commit: be6ba0946c83160f6ce5539e78af12ee294c399b
    Signed-off-by: Manuela Cepeda <cem@adhoc.com.ar>

--- website_sale_advanced_tracking/models/sale_order.py
+++ website_sale_advanced_tracking/models/sale_order.py
@@ -1,3 +1,5 @@
+import json
+
 from odoo import models
 
 
@@ -6,21 +8,30 @@ class SaleOrder(models.Model):
 
     def prepare_purchase_information(self):
         products = []
+        discount = 0.0
+        shipping = 0.0
         for line in self.order_line:
-            products.append(
-                {
-                    "item_id": line.product_id.default_code or line.product_id.id,
-                    "item_name": line.product_id.name,
-                    "item_category": line.product_id.categ_id.name,
-                    "quantity": line.product_uom_qty,
-                    "price": line.price_reduce_taxinc,
-                }
-            )
+            if getattr(line, "is_reward_line", False):
+                discount += abs(line.price_reduce_taxinc * line.product_uom_qty)
+            elif getattr(line, "is_delivery", False):
+                shipping += line.price_reduce_taxinc * line.product_uom_qty
+            else:
+                products.append(
+                    {
+                        "item_id": line.product_id.default_code or line.product_id.id,
+                        "item_name": line.product_id.name,
+                        "item_category": line.product_id.categ_id.name,
+                        "quantity": line.product_uom_qty,
+                        "price": line.price_reduce_taxinc,
+                    }
+                )
         res = {
             "transaction_id": self.id,
             "value": self.amount_total,
             "tax": self.amount_tax,
+            "shipping": shipping,
+            "discount": discount,
             "currency": self.currency_id.name,
             "items": products,
         }
-        return res
+        return json.dumps(res)
