c016555ac3f22505cd96b858b16efb0c386418e0
“Juan <jr@adhoc.com.ar>
2026-06-09 15:05:32 +0000

    [MIG] website_sale_hide_all_prices: migrar a 19.0
    closes ingadhoc/website#460
    
    Signed-off-by: Augusto Weiss <awe@adhoc.com.ar>

--- website_sale_hide_all_prices/static/src/components/add_to_cart_notification.js
+++ website_sale_hide_all_prices/static/src/components/add_to_cart_notification.js
@@ -1,17 +1,11 @@
-/** @odoo-module **/
-
 import { session } from "@web/session";
 import { AddToCartNotification } from "@website_sale/js/notification/add_to_cart_notification/add_to_cart_notification";
-import { CartNotification } from "@website_sale/js/notification/cart_notification/cart_notification";
-import { WarningNotification } from "@website_sale/js/notification/warning_notification/warning_notification";
+import { patch } from "@web/core/utils/patch";
 
-class MyAddToCartNotification extends AddToCartNotification {
-    setup(){
-        super.setup();
-        this.props['website_hide_all_prices'] = session['website_hide_all_prices']
-    }
-}
+patch(AddToCartNotification.prototype, {
+    get websiteHideAllPrices() {
+        return !!session.website_hide_all_prices;
+    },
+});
 
-CartNotification.components = { AddToCartNotification: MyAddToCartNotification , WarningNotification  }
-
-MyAddToCartNotification.template = 'website_sale_hide_all_prices.MyAddToCartNotification'
+AddToCartNotification.template = "website_sale_hide_all_prices.MyAddToCartNotification";
