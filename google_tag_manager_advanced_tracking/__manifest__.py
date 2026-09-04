16213b93d4dc8de0413c1acf7c0f3b72cf42bba2
roboadhoc <roboadhoc@adhoc.com.ar>
2026-09-04 16:45:45 +0000

    [BOT] Bump version: google_tag_manager_advanced_tracking 18.0.1.3.0
    Merged: ingadhoc/website#481

--- google_tag_manager_advanced_tracking/__manifest__.py
+++ google_tag_manager_advanced_tracking/__manifest__.py
@@ -20,7 +20,7 @@
 {
     "name": "Google Tag Manager Advanced Tracking",
     "category": "Website",
-    "version": "18.0.1.2.0",
+    "version": "18.0.1.3.0",
     "author": "ADHOC SA",
     "website": "www.adhoc.com.ar",
     "license": "AGPL-3",
